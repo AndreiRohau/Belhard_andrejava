74560ddc1e8e79f8d0d6fb81ac6925422a484f6f
AndreiRohau <rohau.andrei@gmail.com>
2017-11-07 17:42:04 +0300

    commit by gitgui

---
+++ src/tasksDone/task0/Main.java
@@ -0,0 +1,10 @@
+package tasksDone.task0;
+
+/**
+ * Created by rohau.andrei on 17.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HI man, im done");
+    }
+}
